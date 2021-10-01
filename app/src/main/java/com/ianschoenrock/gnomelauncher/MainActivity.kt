43718c6fb76d7e8402a46ca0f97113f7b4ae8aee
theapphideaway <ianschoenrock@gmail.com>
2021-09-30 21:56:17 -0400

    Removed Whitespace

--- app/src/main/java/com/ianschoenrock/gnomelauncher/MainActivity.kt
+++ app/src/main/java/com/ianschoenrock/gnomelauncher/MainActivity.kt
@@ -32,9 +32,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                    HomePage()
-
-            }
+            HomePage()
+        }
     }
 }
 
